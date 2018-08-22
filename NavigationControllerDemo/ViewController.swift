f8b84b036175d6f6f0b0619eb1a0d4416663445a
Florian Praile <praile.florian@gmail.com>
2018-08-22 16:53:15 +0200

    deist

--- NavigationControllerDemo/ViewController.swift
+++ NavigationControllerDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var countLabel: UILabel!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,17 +22,44 @@ class ViewController: UIViewController {
         }else{
             self.countLabel.text = "Not a nav"
         }
+        
+        print("viewDidLoad \(self.countLabel.text!)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear \(self.countLabel.text!)")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear \(self.countLabel.text!)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear \(self.countLabel.text!)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear \(self.countLabel.text!)")
+    }
+    
+    
     @IBAction func pushAction(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "MainViewController")
-        
+        let vc = storyboard.instantiateViewController(withIdentifier: "MainViewController") as! ViewController
+
         self.navigationController?.pushViewController(vc, animated: true)
         
         // self.present(vc, animated: true)
     }
     
+    
+    deinit {
+        print("ARC has deinit ViewController \(self.countLabel.text!)")
+    }
 
 }
 
